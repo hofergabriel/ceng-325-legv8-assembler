25e61951dd4792921882880668d47b0053b8865f
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-10-30 15:12:36 -0500

    ok

--- Assembler.py
+++ Assembler.py
@@ -17,81 +17,81 @@ class Assembler:
   def __init__(self):
     self.FLAGS = {}
     self.instr2opcode = {
-        "FMULS" : ['R', '00011110001', '000010'], 
-        "FDIVS" : ['R', '00011110001', '000110'], 
-        "FCMPS" : ['R', '00011110001', '001000'],
-        "FADDS" : ['R', '00011110001', '001010'],
-        "FSUBS" : ['R', '00011110001', '001110']
+        'FMULS'  : ['R', '00011110001', '000010'], 
+        'FDIVS'  : ['R', '00011110001', '000110'], 
+        'FCMPS'  : ['R', '00011110001', '001000'],
+        'FADDS'  : ['R', '00011110001', '001010'],
+        'FSUBS'  : ['R', '00011110001', '001110']
 
-        "FMULD" : ['R', '00011110011', '000010'],
-        "FDIVD" : ['R', '00011110011', '000110'],
-        "FCMPD" : ['R', '00011110011', '001000'],
-        "FADDD" : ['R', '00011110011', '001010'],
-        "FSUBD" : ['R', '00011110011', '001110'],
+        'FMULD'  : ['R', '00011110011', '000010'],
+        'FDIVD'  : ['R', '00011110011', '000110'],
+        'FCMPD'  : ['R', '00011110011', '001000'],
+        'FADDD'  : ['R', '00011110011', '001010'],
+        'FSUBD'  : ['R', '00011110011', '001110'],
 
-        "STURB" : []
-        "LDURB" : []
-        "B.cond" : []
-        "STURH" : []
-        "LDURH" : []
-        "AND" : []
-        "ADD" : []
-        "ADDI" : []
-        "ANDI" : []
-        "BL" : []
+        'STURB'  : ['D']
+        'LDURB'  : ['D']
+        'B.cond' : ['CB']
+        'STURH'  : ['D']
+        'LDURH'  : ['D']
+        'AND'    : ['R', '10001010000' ]
+        'ADD'    : ['R', '10001011000' ]
+        'ADDI'   : ['I']
+        'ANDI'   : ['I']
+        'BL'     : ['B']
 
-        "SDIV" : ['R', '10011010110', '000010' ],
-        "UDIV" : ['R', '10011010110', '000011' ],
-        "MUL"  : ['R', '10011011000', '011111' ],
+        'SDIV'   : ['R', '10011010110', '000010' ],
+        'UDIV'   : ['R', '10011010110', '000011' ],
+        'MUL'    : ['R', '10011011000', '011111' ],
 
-        "SMULH" : []
-        "UMULH" : []
-        "ORR" : []
-        "ADDS" : []
-        "ADDIS" : []
-        "ORRI" : []
+        'SMULH'  : ['R', '10011011010']
+        'UMULH'  : ['R', '10011011110']
+        'ORR'    : ['R', '10101010000']
+        'ADDS'   : ['R', '10101011000']
+        'ADDIS'  : ['I']
+        'ORRI'   : ['I']
 
-        "CBS" : []
-        "CBNZ" : []
-        "STURW" : []
-        "LDURSW" : []
-        "STURS" : []
-        "LDURS" : []
-        "STXR" : []
-        "EOR" : []
-        "SUB" : []
-        "SUBI" : []
+        'CBZ'    : ['CB']
+        'CBNZ'   : ['CB']
+        'STURW'  : ['D']
+        'LDURSW' : ['D']
+        'STURS'  : ['R', '10111100000']
+        'LDURS'  : ['R', '10111100010']
+        'STXR'   : ['D']
+        'LDXR'   : ['D']
+        'EOR'    : ['R', '11001010000']
+
+        'SUB'    : ['R', '11001011000']
+        'SUBI'   : ['I']
         
-        "EORI" : []
-        "MOVZ" : []
-        "LSR" : []
-        "LSL" : []
-        "BR" : []
-        "ANDS" : []
-        "SUBS" : []
-        "SUBIS" : []
-        "ANDIS" : []
-        "MOVK" : []
-        "STUR" : []
-        "LDUR" : []
-        "STURD" : []
-        "LDURD" : []
-        
-
-
-
-
-
-
-
+        'EORI'   : ['I']
+        'MOVZ'   : ['IM']
+        'LSR'    : ['R', '11010011010']
+        'LSL'    : ['R', '11010011011']
+        'BR'     : ['R', '11010110000']
+        'ANDS'   : ['R', '11101010000']
+        'SUBS'   : ['R', '11101011000']
+        'SUBIS'  : ['I']
+        'ANDIS'  : ['I']
+        'MOVK'   : ['IM']
+        'STUR'   : ['D']
+        'LDUR'   : ['D']
+        'STURD'  : ['R', '11111100000']
+        'LDURD'  : ['R', '11111100010']
 
         }
+
+  def R(self,line): pass
+  def I(self,line): pass
+  def D(self,line): pass
+  def B(self,line): pass
+  def CB(self,line): pass
+  def IW(self,line): pass
 
   def asm2obj(self, line):
     line=re.split(",| ", line)
 
     print(self.instr2opcode[line[0]][1])
-
     return str(line)
 
 
@@ -107,10 +107,6 @@ def main():
 call main
 """
 main()
-
-
-
-
 
 
 
