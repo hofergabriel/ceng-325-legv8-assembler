9c273ddb2d70520a8f57e4e442852c6950f30db8
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-11-10 16:52:44 -0600

    ok

--- asm.py
+++ asm.py
@@ -9,64 +9,65 @@ import json
 import sys
 
 """
-check this shit out:
-https://www.geeksforgeeks.org/convert-json-to-dictionary-in-python/
-"""
-
-"""
 Assembler
 self.FLAGS: flags register
 """
 class Assembler: 
 
   """ open json file """
-  def __init__(self,filename):
+  def __init__(self,f1,f2):
     self.FLAGS = {}
-    self.i2o = json.load(open(filename))
+    self.i2o = json.load(open(f1))
+    self.b_cond = json.load(open(f2))
 
+  """ immediate value --> binary string """
   def imm2bits(self,word): 
     if word[0]=='#' : return str(bin(int(word[1:]))[2:])
     return str(bin(int(word,16))[2:])
 
+  """ maps branch-conditional instructions to their code """
+  def B_cond(self,word):
+    if word[0]=='B' : return self.b_cond[word]
+    else: return '-'
 
   """ list of tokens --> binary """
   def R(self,line): 
-    return self.i2o[line[0]][1] + ' ' + \
-        str(bin(int(line[3][1:]))[2:]).zfill(5) + ' ' + \
-        self.i2o[line[0]][2] + ' ' + \
-        str(bin(int(line[2][1:]))[2:]).zfill(5) + ' ' + \
+    return self.i2o[line[0]][1] + \
+        str(bin(int(line[3][1:]))[2:]).zfill(5) + \
+        self.i2o[line[0]][2] + \
+        str(bin(int(line[2][1:]))[2:]).zfill(5) + \
         str(bin(int(line[1][1:]))[2:]).zfill(5)
 
   def I(self,line): 
-    return self.i2o[line[0]][1] + ' ' + \
-        str(bin(int(line[3][1:]))[2:]).zfill(12) + ' ' + \
-        str(bin(int(line[2][1:]))[2:]).zfill(5) + ' ' + \
-        str(bin(int(line[1][1:]))[2:]).zfill(5) + ' ' 
+    return self.i2o[line[0]][1] + \
+        str(bin(int(line[3][1:]))[2:]).zfill(12) + \
+        str(bin(int(line[2][1:]))[2:]).zfill(5) +  \
+        str(bin(int(line[1][1:]))[2:]).zfill(5)   
 
   def D(self,line): 
-    return self.i2o[line[0]][1] + ' ' + \
-        str(bin(int(line[3][1:]))[2:]).zfill(9) + ' ' + \
-        "--" + \
-        str(bin(int(line[2][1:]))[2:]).zfill(5) + ' ' + \
-        str(bin(int(line[1][1:]))[2:]).zfill(5) + ' ' 
+    return self.i2o[line[0]][1] + \
+        str(bin(int(line[3][1:]))[2:]).zfill(9) + \
+        " 00 " + \
+        str(bin(int(line[2][1:]))[2:]).zfill(5) + \
+        str(bin(int(line[1][1:]))[2:]).zfill(5) 
 
   def B(self,line): 
-    return self.i2o[line[0]][1] + ' ' + \
+    return self.i2o[line[0]][1] + \
         str(bin(int(line[1][1:]))[2:]).zfill(26)
 
+  # -- TODO -- 
   def CB(self,line): 
     #str(bin(int(line[2],16))[2:]).zfill(19) + ' ' + \
-    return self.i2o[line[0]][1] + ' ' + \
-        "--" + \
-        self.imm2bits(line[2]).zfill(19) + ' ' + \
-        str(bin(int(line[1][1:]))[2:]).zfill(5) + ' ' 
+    return self.i2o[line[0]][1] + \
+        self.imm2bits(line[2]).zfill(19) + \
+        str(bin(int(line[1][1:]))[2:]).zfill(5)
 
+  # -- TODO -- 
   def IW(self,line): 
-    #str(bin(int(line[2],16))[2:]).zfill(16) + ' ' + \
-    return self.i2o[line[0]][1] + ' ' + \
-        "--" + \
-        self.imm2bits(line[2]).zfill(16) + ' ' + \
-        str(bin(int(line[1][1:]))[2:]).zfill(5) + ' ' 
+    return self.i2o[line[0]][1] + \
+        str(bin(int(line[4])//16)[2:]).zfill(2) + \
+        self.imm2bits(line[2]).zfill(16) + \
+        str(bin(int(line[1][1:]))[2:]).zfill(5) 
 
   """ whole instruction string --> binary
       determines which type of instruction """
@@ -74,7 +75,6 @@ class Assembler:
     line=list(filter(lambda i : i!='', re.split(",| |\[|\]|\n", line)))
 
     print(line)
-
     fmt = self.i2o[line[0]][0]
     if fmt == 'R':    return self.R(line)
     elif fmt == 'I':  return self.I(line)
@@ -83,13 +83,22 @@ class Assembler:
     elif fmt == 'CB': return self.CB(line)
     else:             return self.IW(line)
 
+""" puts spaces in the string every four bits """
+def nibbles(s):
+  ret=""
+  for i in range(len(s)):
+    ret+=s[i]
+    if (i+1)%4==0:
+      ret+=' '
+  return ret
+
 """ Entry point """
 def main():
-  assembler = Assembler(sys.argv[1])
+  assembler = Assembler(sys.argv[1],sys.argv[2])
   while(True):
     s=input()
     if s=="quit": break
-    print(assembler.asm2obj(s))
+    print(nibbles(assembler.asm2obj(s)))
 
 """ call main """
 main()
